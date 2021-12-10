669daa23cf8e4e130a23facbc8f86812d8688176
KilroyWasHere-cs-j <59028838+KilroyWasHere-cs-j@users.noreply.github.com>
2021-12-10 16:02:40 -0500

    Update Constants.java
    NW and SW are running in the same direction.

--- src/main/java/org/frc5687/swerve/Constants.java
+++ src/main/java/org/frc5687/swerve/Constants.java
@@ -17,7 +17,7 @@ public class Constants {
         public static final double LENGTH = 0.6223;
         public static final Translation2d FRONT_LEFT_POSITION =
                 new Translation2d(WIDTH / 2.0, LENGTH / 2.0);
-        public static final double FRONT_LEFT_ENCODER_OFFSET = -0.071; // radians
+        public static final double FRONT_LEFT_ENCODER_OFFSET = Math.PI * 1.9; // radians
         public static final Translation2d FRONT_RIGHT_POSITION =
                 new Translation2d(WIDTH / 2.0, -LENGTH / 2.0);
         public static final double FRONT_RIGHT_ENCODER_OFFSET = -1.01; // radians
@@ -26,7 +26,7 @@ public class Constants {
         public static final double BACK_RIGHT_ENCODER_OFFSET = Math.PI * 1.2; // radians
         public static final Translation2d BACK_RIGHT_POSITION =
                 new Translation2d(-WIDTH / 2.0, -LENGTH / 2.0);
-        public static final double BACK_LEFT_ENCODER_OFFSET = 0.585 + Math.PI; // radians
+        public static final double BACK_LEFT_ENCODER_OFFSET = Math.PI / 1.3; // radians
 
         public static final double DEADBAND = 0.1;
 
