99178689601c7f7e6dde582b252794f63aa2c8b8
KilroyWasHere-cs-j <59028838+KilroyWasHere-cs-j@users.noreply.github.com>
2021-12-13 18:43:28 -0500

    Update RobotMap.java
    Removing comments

--- src/main/java/org/frc5687/swerve/RobotMap.java
+++ src/main/java/org/frc5687/swerve/RobotMap.java
@@ -12,12 +12,12 @@ public class RobotMap {
     public static class CAN {
 
         public static class TALONFX {
-            public static final int BL_RIGHT_FALCON = 4; // real 3
-            public static final int BL_LEFT_FALCON = 3; // real 4
+            public static final int BL_RIGHT_FALCON = 4;
+            public static final int BL_LEFT_FALCON = 3;
             public static final int BR_RIGHT_FALCON = 6;
             public static final int BR_LEFT_FALCON = 5;
-            public static final int FL_RIGHT_FALCON = 1; // real 5
-            public static final int FL_LEFT_FALCON = 2; // real 6
+            public static final int FL_RIGHT_FALCON = 1;
+            public static final int FL_LEFT_FALCON = 2;
             public static final int FR_RIGHT_FALCON = 7;
             public static final int FR_LEFT_FALCON = 8;
         }
