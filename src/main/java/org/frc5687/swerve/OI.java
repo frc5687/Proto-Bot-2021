082f3c46bb8ae996dcd1c95fbec6273596d58a2f
KilroyWasHere-cs-j <59028838+KilroyWasHere-cs-j@users.noreply.github.com>
2021-12-13 23:15:42 -0500

    Joystick FIx
    Added so fixes to joystick instead of gamepad. The joysticks don't do the thing. The translation rotates and goes against eachother.

--- src/main/java/org/frc5687/swerve/OI.java
+++ src/main/java/org/frc5687/swerve/OI.java
@@ -1,56 +1,52 @@
 /* Team 5687 (C)2020-2021 */
 package org.frc5687.swerve;
 
-import static org.frc5687.swerve.Constants.DriveTrain.*;
-import static org.frc5687.swerve.util.Helpers.*;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj2.command.button.Button;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import org.frc5687.swerve.subsystems.DriveTrain;
-import org.frc5687.swerve.util.AxisButton;
-import org.frc5687.swerve.util.Gamepad;
+import org.frc5687.swerve.util.Helpers;
 import org.frc5687.swerve.util.OutliersProxy;
 
 public class OI extends OutliersProxy {
-    protected Joystick translation;
-    protected Joystick rotation;
+    private Joystick translate;
+    private Joystick rotation;
     //Test
 
     private double yIn = 0;
     private double xIn = 0;
 
     public OI() {
-        translation = new Joystick(0);
-        rotation = new Joystick(5);
+        translate = new Joystick(1);
+        rotation = new Joystick(0);
     }
 
     public void initializeButtons(DriveTrain driveTrain) {}
 
     public double getDriveY() {
-        yIn = getSpeedFromAxis(translation, translation.getYChannel());
-        //yIn = getSpeedFromAxis(_driverGamepad, Gamepad.Axes.LEFT_Y.getNumber());
-        yIn = applyDeadband(yIn, Constants.DriveTrain.DEADBAND);
+        yIn = getSpeedFromAxis(translate, translate.getYChannel());
+        //        yIn = getSpeedFromAxis(_driverGamepad, Gamepad.Axes.LEFT_Y.getNumber());
+        yIn = Helpers.applyDeadband(yIn, Constants.DriveTrain.DEADBAND);
+
         double yOut = yIn / (Math.sqrt(yIn * yIn + (xIn * xIn)) + Constants.EPSILON);
         yOut = (yOut + (yIn * 2)) / 3.0;
-        return yOut;  
+        return yOut;
     }
 
     public double getDriveX() {
-        xIn = -getSpeedFromAxis(translation, translation.getXChannel());
-        //xIn = -getSpeedFromAxis(_driverGamepad, Gamepad.Axes.LEFT_X.getNumber());
-        xIn = applyDeadband(xIn, Constants.DriveTrain.DEADBAND);
+        xIn = -getSpeedFromAxis(translate, translate.getXChannel());
+        xIn = Helpers.applyDeadband(xIn, Constants.DriveTrain.DEADBAND);
+
         double xOut = xIn / (Math.sqrt(yIn * yIn + (xIn * xIn)) + Constants.EPSILON);
         xOut = (xOut + (xIn * 2)) / 3.0;
         return xOut;
     }
 
     public double getRotationX() {
-        double speed = getSpeedFromAxis(rotation, rotation.getYChannel());
-        speed = applyDeadband(speed, Constants.DriveTrain.DEADBAND);
-        return speed;
+        return rotation.getX();
     }
 
     protected double getSpeedFromAxis(Joystick joystick, int axisNumber) {
+        metric("Axis", joystick.getRawAxis(axisNumber));
+        metric("Axis port", axisNumber);
         return joystick.getRawAxis(axisNumber);
     }
 
